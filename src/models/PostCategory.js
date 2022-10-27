a8b0b4d27d58cb14c0fee3c416de572bcfa460c6
David <david.lourenco94@gmail.com>
2022-10-27 14:44:02 -0300

    Requisito 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define(
+    "PostCategory",
+    {
+      postId: {
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      categoryId: {
+        primaryKey: true,
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      underscored: true,
+      timestamps: false,
+      tableName: "posts_categories",
+    }
+  );
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: "categories",
+      through: PostCategory,
+      foreignKey: "categoryId",
+      otherKey: "postId",
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: "posts",
+      through: PostCategory,
+      foreignKey: "postId",
+      otherKey: "categoryId",
+    });
+  };
+
+  return PostCategory;
+};
